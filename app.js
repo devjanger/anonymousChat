afe727159a2da5999a2a1d733129f58d25aac8d7
JangER <55939719+Logic-01001010@users.noreply.github.com>
2022-01-01 03:03:49 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -6,35 +6,118 @@ const moment = require('moment');
 
 app.use(express.static('public'));
 
+var users = {};
+var names = {};
+
+
+function isExistUname(name){
+
+  if( names[name] == undefined )
+    return false;
+  else
+    return true;
+
+}
+
+function removeUser(user){
+
+  if( users[user] != undefined ){
+    var n = users[ user ];
+    delete users[ user ];
+    delete names[ n['uname'] ];
+    console.log(`${n['uname']}님을 유저 목록애서 제거하였습니다.`);
+  } else {
+    console.log('유저 목록에 존재하지 않습니다.');
+  }
+
+}
+
+
+function registrationUser(name, sockid){
+      users[ sockid ] = { 'uname': name };
+      names[ name ] = { 'sockid': sockid };
+}
+
+
+function getUserName(sockid){
+      var u = users[ sockid ];
+      return u['uname'];;
+}
+
+
+function filter( str ){
+  str = str.replace(/</gi, "&lt");
+  str = str.replace(/>/gi, "&gt");
+  return str;
+}
+
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
 
-var users = {};
 
 io.on("connection", (socket) => {
 
-    console.log(socket.id);
+  console.log(socket.id);
   console.log("유저가 접속 했습니다.");
-  io.to(socket.id).emit('chat message', 'for your eyes only');
+  
+
 
   socket.on("chat message", (msg) => {
-      var payload = msg;
-      let mm = moment();
-      payload['date'] = mm.format('h:mm:ss A');
-      io.emit("chat message", msg);
+
+      if( users[ socket.id ] != undefined ){
+
+          var payload = msg;
+          payload['uname'] = getUserName(socket.id);
+          let mm = moment();
+          payload['date'] = mm.format('h:mm:ss A');
+          payload['message'] = filter( payload['message'] );
+
+          io.emit("chat message", payload);
+
+      }
+
+
   });
 
+
+
+  socket.on("check name", (msg)=>{
+
+    var name = msg;
+
+    if( !isExistUname( filter( name ) ) )
+      io.to(socket.id).emit('check name', {'success': true, 'message': '사용 가능한 닉네임입니다.'});    
+    else
+      io.to(socket.id).emit('check name', {'success': false, 'message': '사용 불가능한 닉네임입니다.'});    
+  
+  })
+
   socket.on("set name", (msg) =>{
-        if( users[ msg ] == undefined )
-            io.to(socket.id).emit('set name', '사용 가능!');
+
+    var name = msg;
+    
+    if( isExistUname( filter( name ) ) ){
+      io.to(socket.id).emit('check name', {'success': false, 'message': '사용 불가능한 닉네임입니다.'});    
+    } else{
+      registrationUser( filter( name ), socket.id );
+      io.to(socket.id).emit('set name', {'success': true, 'uname': name});
+      io.to(socket.id).emit('chat message', {'type': 'enter_me'});
+      socket.broadcast.emit('chat message', {'type': 'enter', 'uname': name});
+    }
   })
 
   socket.on("disconnect", () => {
-    console.log(socket.id)
-    console.log("유저가 나갔습니다.");
+    console.log(`유저가 나갔습니다.`);
+    
+    if( users[ socket.id ] != undefined ){
+      var uname = getUserName( socket.id );  
+      io.emit("chat message", {'type': 'left', 'uname': uname});
+      removeUser(socket.id);
+    }
+
   });
 
 });
--- public/script.js
+++ public/script.js
@@ -1,59 +1,108 @@
 $( ()=>{
 
-    username = $('.username').val();
+    // $('.init_container').remove();
 
-    var payload = {"type": null, "uname": null, "message": null, "date": null};
+    username = null;
+
+    var payload = {"type": null, "message": null, "date": null};
 
     const socket = io();
 
+
+    ////// 초기 설정 화면 //////
+    socket.on('check name', (msg)=>{
+        if(msg['success']){
+            alert(msg['message']);
+            $('.enter_btn').attr('disabled', false);
+        } else{
+            alert(msg['message']);
+            $('.enter_btn').attr('disabled', true);
+        }
+    })
     socket.on('set name', (msg)=>{
-        console.log('set name 결과', msg);
+        if( msg['success'] ){
+            console.log('채팅방 접속');
+            $('.init_container').remove();
+        } else {
+            alert(msg['message']);
+            $('.enter_btn').attr('disabled', true);
+        }
     })
 
-    $('.init_btn').on('click', ()=>{
-        console.log('클릭')
-        var uname = $('.init_input').val();
-        
-        socket.emit('set name', uname);
+    $('.init_input').keydown( ()=>{
+        $('.enter_btn').attr('disabled', true);
+    })
 
+    // 닉네임 존재 확인
+    $('.check_btn').on('click', ()=>{
+        var uname = $('.init_input').val();
+        socket.emit('check name', uname);
     });
+
+    // 입장
+    $('.enter_btn').on('click', ()=>{
+        var uname = $('.init_input').val();
+        socket.emit('set name', uname);
+        uname = uname.replaceAll('<', "&lt");
+        uname = uname.replaceAll('>', "&gt");
+        
+        username = uname;
+    });
+    ///////////////////////////
 
 
     socket.on('chat message', (msg)=>{
 
         console.log("수신: ", msg);
         var {type, uname, message, date} = msg;
-        
+        console.log(msg);
         if( type == 'chat' ){
             $('.list_box').append(
-                `<ul class="chat_item">
+                `
+                ${ (uname == username) ? '<ul class="chat_item me">' : '<ul class="chat_item">' }
                     <li>
-                        <span class="date">${date}</span>
                         <span class="uname">${uname}</span>
                         <span class="message">${message}</span>
+                        <span class="date">${date}</span>
                     </li>
                 </ul>
                 `
             );
         }
-        else if( type == 'change_uname' ){
+        else if( type == 'left' ){
             $('.list_box').append(
-                `<ul class="chat_item">
-                    <li>
-                        <span class="date">알림! ${date}</span>
-                        <span class="message">${message}</span>
-                    </li>
-                </ul>
                 `
-            );
+                 <div class='alert_message'>${uname}님이 퇴장하였습니다.</div>
+                `
+            );           
         }
+        else if( type == 'enter' ){
+            $('.list_box').append(
+                `
+                 <div class='alert_message'>${uname}님이 접속하였습니다.</div>
+                `
+            );                 
+        }
+        else if( type == 'enter_me' ){
+            $('.list_box').append(
+                `
+                 <div class='alert_message'>채팅방에 접속하였습니다.</div>
+                `
+            );                             
+        }
+   
+        $('.list_box').scrollTop( $('.list_box')[0].scrollHeight);
+
     });
 
 
     var send = ()=>{
         send_message = $('.input_box').val();
+        
+        if( send_message == null || send_message == "" ){
+            return;
+        }
         payload["type"] = "chat";
-        payload["uname"] = username;
         payload["message"] = send_message;
 
         socket.emit('chat message', payload);
@@ -70,18 +119,7 @@ $( ()=>{
     } )
 
 
-    $('.change_uname').on("click", ()=>{
-        var current_uname = username;
-        var changed_uname = $('.username').val();
 
-        username = changed_uname;
-
-        payload["type"] = "change_uname";
-        payload["uname"] = current_uname;
-        payload["message"] = `${current_uname}님이 ${changed_uname}으로 닉네임을 수정했습니다.`;
-
-        socket.emit('chat message', payload);
-    })
 
 
 } )
